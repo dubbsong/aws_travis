1de03fc99e15bd3463a54021ded7960f005d86db
dubbsong <dubbsong@gmail.com>
2018-02-14 11:24:45 +0900

    aa

--- test.js
+++ test.js
@@ -1,10 +1,1 @@
-const express = require('express');
-const app = express();
-
-app.get('/', (req, res) => {
-  res.json('Hi');
-});
-
-const port = process.env.PORT || 3000;
-
-app.listen(port, () => console.log(`server listen ${port}`));
+console.log('hi');
